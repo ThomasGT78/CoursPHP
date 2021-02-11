2e9b9a1c40f25c41890f00b3d3620a21eefd7e00
thomas <thomas.garot@gmail.com>
2021-02-11 19:34:11 +0100

    Mise à jour de l'Index

--- index.php
+++ index.php
@@ -1,14 +1,4 @@
-<?php
-// Déclaration des variables
-$name = "Tom";
-$age = 33;
-$fruit = "pomme";
-/*
-print et echo servent tous les deux à afficher du texte
- */
-print "Hello";
-echo " world";
-?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -16,7 +6,7 @@ echo " world";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Index</title>
 </head>
 
 <body>
